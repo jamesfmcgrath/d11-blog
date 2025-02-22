c90ead7c0487b60f27f544f8097e45ae3d5e7664
James McGrath <james@mcgrathmedia.com>
2025-02-22 10:34:19 -0800

    Olivero child theme test

---
+++ web/core/themes/olivero_theme_sub/js/script.js
@@ -0,0 +1,7 @@
+(function (Drupal) {
+  Drupal.behaviors.myThemeBehavior = {
+    attach: function (context, settings) {
+      console.log("My Olivero Child Theme Loaded");
+    },
+  };
+})(Drupal);
